a69764c49a68d38e5acb4dafac8d122c6f1cdb3d
vvwenner <torifreestyle@hotmail.com>
2016-09-01 17:44:39 -0500

    All problems done, R file
    Did a R file because the doc, docx was not allowing me to commit to
    master

---
+++ assignment1R.R
@@ -0,0 +1,47 @@
+#1.0
+#R version 3.1.2 (2014-10-31) -- "Pumpkin Helmet"
+#Copyright (C) 2014 The R Foundation for Statistical Computing
+#Platform: x86_64-w64-mingw32/x64 (64-bit)
+
+#1.1
+2015^(1/3)
+#12.63063
+
+#1.2
+abs(5.7-6.8)/.58
+#1.896552
+
+#1.3
+a=1:12
+a
+#1  2  3  4  5  6  7  8  9 10 11 12
+
+#1.4
+b = c(1, 3, 5, 7, 9, 11)
+b
+#1  3  5  7  9 11
+
+#1.5
+c = seq(1,11, 2)
+c
+#1  3  5  7  9 11
+
+#1.6
+ln.a = log(a)
+ln.a
+#0.0000000 0.6931472 1.0986123 1.3862944 1.6094379 1.7917595 1.9459101 2.0794415 2.1972246 2.3025851 2.3978953 2.4849066
+
+#1.7
+c^2 #squares
+#1   9  25  49  81 121
+sqrt(c) #square root
+#1.000000 1.732051 2.236068 2.645751 3.000000 3.316625
+
+#1.8
+?sd
+#Directed to a website on the Standard Deviation function.The website includes a description, usage, arguments, details, and examples on the Standard Deviation function.   
+
+#1.9
+name <- 'Tori'
+paste('My name is ', name)
+#"My name is  Tori"
